19ebd4cd585730791f04046783b1da62f6f8ef7a
FrancescoSTL <francescosapolizzi@gmail.com>
2016-07-30 12:26:52 -0700

    Fix syntax error

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-        res.render('index.html', { title : "Sample Node Express + Nunjucks app");
+        res.render('index.html', { title : "Sample Node Express + Nunjucks app" });
 });
 
 module.exports = router;
