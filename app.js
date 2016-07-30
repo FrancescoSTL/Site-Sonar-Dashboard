26978db897201c6b5f6442ba7f273d109110cde6
FrancescoSTL <francescosapolizzi@gmail.com>
2016-07-30 13:35:09 -0700

    Add body parser to app.js

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ app.use('/', routes);
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.bodyParser());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
