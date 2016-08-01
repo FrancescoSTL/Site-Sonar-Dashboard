29935a8d7b8adbfd512db26c197dc4cbd419ff98
purukaushik <purukaushik@asu.edu>
2016-08-01 10:35:10 -0700

    Review fixes, renaming net to host, spaces instead of tabs

--- routes/index.js
+++ routes/index.js
@@ -10,110 +10,110 @@ var url = 'mongodb://heroku_803m4q4j:1f1t2tc0ih11omaekmln78tkcn@ds031995.mlab.co
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	try {
-		MongoClient.connect(url, function(err, db) {
-			var benchmarkDB = db.collection('benchmark_logs');
+    try {
+        MongoClient.connect(url, function(err, db) {
+            var benchmarkDB = db.collection('benchmark_logs');
 
-			try {
-				benchmarkDB.find({},{sort: {assetCompleteTime: -1}}).toArray(function(err, assetLoadTimes) {
-					benchmarkDB.find().count(function (err, total) {
-						var millisecondTotal = 0;
-						for (var record in assetLoadTimes) {
-							millisecondTotal += parseInt(record);
-						}
+            try {
+                benchmarkDB.find({},{sort: {assetCompleteTime: -1}}).toArray(function(err, assetLoadTimes) {
+                    benchmarkDB.find().count(function (err, total) {
+                        var millisecondTotal = 0;
+                        for (var record in assetLoadTimes) {
+                            millisecondTotal += parseInt(record);
+                        }
 
-						// calculate our total processing time in minutes, and round to 2 decimal places
-						var minutesTotal = Math.round((millisecondTotal * .000016667) * 100) / 100;
+                        // calculate our total processing time in minutes, and round to 2 decimal places
+                        var minutesTotal = Math.round((millisecondTotal * .000016667) * 100) / 100;
 
-						res.render('index.html', { records : assetLoadTimes.slice(0,99), count: total, totalTime: minutesTotal});
+                        res.render('index.html', { records : assetLoadTimes.slice(0,99), count: total, totalTime: minutesTotal});
 
-						db.close();
-					});
-				});
-			} catch (e) {
-					console.log("Could not find records in database " + e);
-			}
-		});
-	} catch (e) {
-		console.log("Could not connect to MongoDb " + e);
-	}
+                        db.close();
+                    });
+                });
+            } catch (e) {
+                    console.log("Could not find records in database " + e);
+            }
+        });
+    } catch (e) {
+        console.log("Could not connect to MongoDb " + e);
+    }
 });
 
 /* Group by ad networks display. */
-router.get('/groupBy', function(req,res){
-	try{
-		MongoClient.connect(url, function(err, db){
-			var benchmarkDB = db.collection('benchmark_logs');
-			try{
-				benchmarkDB.aggregate(
-					[
-						{
-							$group: {
-								_id : "$adNetworkUrl",
-								avgLoadTime : { $avg : "$assetCompleteTime"}
-							}
-						},
-						{
-							$sort: {
-								avgLoadTime: -1
-							}
-						},
-						{
-							$project:
-							{
-								_id: "$_id",
-								avgLoadTime:
-								{
-									$divide:[
-										{$subtract:[
-											{$multiply:['$avgLoadTime',1000]},
-											{$mod:[{$multiply:['$avgLoadTime',1000]}, 1]}
-										]},
-										1000
-									]
-								}
-							}
-						}
-					]
-				).toArray(function (err, avgLoadTimes){
-					benchmarkDB.find().count(function (err, total) {
-						res.render('groupByAdNets.html', { records : avgLoadTimes.slice(0,99)});
-						db.close();
-					});
-				});
-			} catch (e) {
-				console.log("Could not connect to MongoDb " + e);
-			}
-		});
-	} catch (e) {
-		console.log("Could not connect to MongoDb " + e) ;
-	}
+router.get('/groupByAdHost', function(req,res) {
+    try {
+        MongoClient.connect(url, function(err, db) {
+            var benchmarkDB = db.collection('benchmark_logs');
+            try {
+                benchmarkDB.aggregate(
+                    [
+                        {
+                            $group: {
+                                _id : "$adNetworkUrl",
+                                avgLoadTime : { $avg : "$assetCompleteTime"}
+                            }
+                        },
+                        {
+                            $sort: {
+                                avgLoadTime: -1
+                            }
+                        },
+                        {
+                            $project:
+                            {
+                                _id: "$_id",
+                                avgLoadTime:
+                                {
+                                    $divide:[
+                                        {$subtract:[
+                                            {$multiply:['$avgLoadTime',1000]},
+                                            {$mod:[{$multiply:['$avgLoadTime',1000]}, 1]}
+                                        ]},
+                                        1000
+                                    ]
+                                }
+                            }
+                        }
+                    ]
+                ).toArray(function (err, avgLoadTimes){
+                    benchmarkDB.find().count(function (err, total) {
+                        res.render('groupByAdHost.html', { records : avgLoadTimes.slice(0,99)});
+                        db.close();
+                    });
+                });
+            } catch (e) {
+                console.log("Could not connect to MongoDb " + e);
+            }
+        });
+    } catch (e) {
+        console.log("Could not connect to MongoDb " + e) ;
+    }
 });
 /* POST for data logging to Mongo */
 router.post('/log', function(req, res) {
-	// TODO: parse data and send to Mongo
-	var assetLoadTimes = req.body.assets;
-	try {
-		MongoClient.connect(url, function(err, db) {
-			if(err) {
-				console.log("Could not connect to MongoDb " + err);
-			} else {
-				var benchmarkDB = db.collection('benchmark_logs');
+    // TODO: parse data and send to Mongo
+    var assetLoadTimes = req.body.assets;
+    try {
+        MongoClient.connect(url, function(err, db) {
+            if(err) {
+                console.log("Could not connect to MongoDb " + err);
+            } else {
+                var benchmarkDB = db.collection('benchmark_logs');
 
-				try {
-					benchmarkDB.insertMany(assetLoadTimes).then(function(r) {
-						// let the client know we've received their data
-				  		res.send("Data successfully recieved by Ultra-Lightbeam. Thanks :-).");
-					    db.close();
-					});
-				} catch (e) {
-					console.log("Could not log to database " + e);
-				}
-			}
-		});
-	} catch (e) {
-		console.log("Could not connect to MongoDb " + e);
-	}
+                try {
+                    benchmarkDB.insertMany(assetLoadTimes).then(function(r) {
+                        // let the client know we've received their data
+                        res.send("Data successfully recieved by Ultra-Lightbeam. Thanks :-).");
+                        db.close();
+                    });
+                } catch (e) {
+                    console.log("Could not log to database " + e);
+                }
+            }
+        });
+    } catch (e) {
+        console.log("Could not connect to MongoDb " + e);
+    }
 });
 
 module.exports = router;
