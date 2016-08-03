f65cc69c462ec64a5802f4418aed3f3d245343bb
FrancescoSTL <francescosapolizzi@gmail.com>
2016-08-02 20:42:10 -0700

    Don't sort top 100

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res) {
             var benchmarkDB = db.collection('benchmark_logs');
 
             try {
-                benchmarkDB.find({},{sort: {assetCompleteTime: -1}}).toArray(function(err, assetLoadTimes) {
+                benchmarkDB.find().toArray(function(err, assetLoadTimes) {
                     benchmarkDB.find().count(function (err, total) {
                         var millisecondTotal = 0;
                         for (var record in assetLoadTimes) {
